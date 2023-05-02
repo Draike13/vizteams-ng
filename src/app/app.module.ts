e6ebd0d377d19f5050b3147225d0736f4c9f045b
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-05-01 20:05:33 -0500

    5-1-23
    Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>
    Co-authored-by: Joe Letner <jrletner@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TeamlistComponent } from './teamlist/teamlist.component';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TeamlistComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
