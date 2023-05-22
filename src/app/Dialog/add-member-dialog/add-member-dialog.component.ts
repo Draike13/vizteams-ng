cb59892cfeb8bff4504511e3a68a726d11732eba
Draike13 <tmbiri91@gmail.com>
2023-05-22 11:19:12 -0500

    test commit to check theory on CORS ERR

--- src/app/Dialog/add-member-dialog/add-member-dialog.component.ts
+++ src/app/Dialog/add-member-dialog/add-member-dialog.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-add-member-dialog',
@@ -7,7 +9,19 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./add-member-dialog.component.scss'],
 })
 export class AddMemberDialogComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  picsum$: Subject<any>;
+  getPicsum: any;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private http: HttpClient
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http
+      .get('https://picsum.photos/v2/list?page=2&limit=100/100.jpg')
+      .subscribe((res) => {
+        console.log(res);
+        this.getPicsum = res;
+      });
+  }
 }
