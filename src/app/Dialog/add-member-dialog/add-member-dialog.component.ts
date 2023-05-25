c8e1d2c54a4e367c5ecdb61e426097f4f2330691
Draike13 <tmbiri91@gmail.com>
2023-05-25 18:42:06 -0500

    add requirements to add member form

--- src/app/Dialog/add-member-dialog/add-member-dialog.component.ts
+++ src/app/Dialog/add-member-dialog/add-member-dialog.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Inject } from '@angular/core';
-import { Form, FormControl } from '@angular/forms';
+import { Form, FormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
 import { Subject } from 'rxjs';
@@ -35,10 +35,13 @@ export class AddMemberDialogComponent implements OnInit {
     private databaseService: DatabaseService,
     private infoService: InfoService
   ) {
-    this.fnameControl = new FormControl();
-    this.lnameControl = new FormControl();
-    this.titleControl = new FormControl();
-    this.teamControl = new FormControl();
+    this.fnameControl = new FormControl('', Validators.required);
+    this.lnameControl = new FormControl('', Validators.required);
+    this.titleControl = new FormControl('', Validators.required);
+    this.teamControl = new FormControl(
+      this.data.selectedTeam.id,
+      Validators.required
+    );
   }
 
   ngOnInit(): void {
@@ -70,7 +73,7 @@ export class AddMemberDialogComponent implements OnInit {
       fname: this.fnameControl.value,
       lname: this.lnameControl.value,
       title: this.titleControl.value,
-      team_id: this.data.selectedTeam.id,
+      team_id: this.teamControl.value,
       img_url: this.img_url,
     };
     this.databaseService.newTeamMember(newMember).subscribe(() => {
@@ -79,5 +82,30 @@ export class AddMemberDialogComponent implements OnInit {
       this.infoService.infoDisplay$.next(undefined);
     });
     console.log(newMember);
+  }
+
+  getfnameErrorMessage() {
+    if (this.fnameControl.hasError('required')) {
+      return 'First name required';
+    }
+    return ''; // Default return value when the condition is false
+  }
+  getlnameErrorMessage() {
+    if (this.lnameControl.hasError('required')) {
+      return 'Last name required';
+    }
+    return ''; // Default return value when the condition is false
+  }
+  gettitleErrorMessage() {
+    if (this.titleControl.hasError('required')) {
+      return 'Title required';
+    }
+    return ''; // Default return value when the condition is false
+  }
+  getteamidErrorMessage() {
+    if (this.teamControl.hasError('required')) {
+      return 'Team required';
+    }
+    return ''; // Default return value when the condition is false
   }
 }
